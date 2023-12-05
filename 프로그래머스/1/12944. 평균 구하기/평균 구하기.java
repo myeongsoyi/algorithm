7cd516d909e3fd58acee10ac53361c2ef5c61f10
myeongsoyi <66073336+myeongsoyi@users.noreply.github.com>
2023-12-05 22:38:18 +0900

    [level 1] Title: 평균 구하기, Time: 0.07 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double solution(int[] arr) {
+        int total = 0;
+        for(int num : arr) total += num;
+        double answer = (double) total/arr.length;
+        return answer;
+    }
+}
